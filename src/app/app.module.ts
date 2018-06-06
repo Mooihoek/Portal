fcd9ab91d8a836d6b457db86733676957b06a5b9
Chris <cherryc@vmware.com>
2018-06-06 22:48:06 +1200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,25 @@ import { AppComponent } from './app.component';
 import { ROUTING } from "./app.routing";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { DashboardsComponent } from './dashboards/dashboards.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { ToolsComponent } from './tools/tools.component';
+import { TestComponent } from './test/test.component';
+import { Routes } from "@angular/router";
+
+
 
 @NgModule({
     declarations: [
         AppComponent,
         AboutComponent,
-        HomeComponent
+        HomeComponent,
+        DashboardsComponent,
+        FaqsComponent,
+        ToolsComponent,
+        TestComponent,
+       
+       
     ],
     imports: [
         BrowserAnimationsModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,12 +8,21 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardsComponent } from './dashboards/dashboards.component';
+import { ToolsComponent } from './tools/tools.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { TestComponent } from './test/test.component';
+
 
 
 export const ROUTES: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', component: AboutComponent},
+    {path: 'dashboards', component: DashboardsComponent},
+    {path: 'tools', component: ToolsComponent},
+    {path: 'faqs', component: FaqsComponent},
+    {path: 'test', component:TestComponent},
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(ROUTES);

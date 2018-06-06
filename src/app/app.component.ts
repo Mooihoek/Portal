2798d4bfb262d4c73ea67382374a2912e7e7c16e
Chris <cherryc@vmware.com>
2018-06-07 09:58:24 +1200

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    constructor(private router: Router) {
-    }
+  about = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((val: RouterEvent) => {
+      if (val.url === '/about') {
+        this.about = true;
+      } else {
+        this.about = false;
+      }
+    });
+  }
 }
